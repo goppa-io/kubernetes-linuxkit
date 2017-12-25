fe08de3a5e373b7880ff36026070c895fc06ec3b
denverwilliams <denver@debian.nz>
2017-12-25 11:25:00 +0000

    Start sshd with debug

--- pkg/sshd/etc/init.d/ssh.sh
+++ pkg/sshd/etc/init.d/ssh.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-KEYS=$(find /etc/ssh -name 'ssh_host_*_key')
-[ -z "$KEYS" ] && ssh-keygen -A >/dev/null 2>/dev/null
-
-exec /usr/sbin/sshd -e -f /etc/ssh/sshd_config
+exec /usr/sbin/sshd -e -f /etc/ssh/sshd_config -dD > /var/log/sshd.log 2>&1 &
