907fa3a26124ff0d7040e2421ade06f4e6ed00b8
denverwilliams <denver@debian.nz>
2018-02-11 12:21:33 +0000

    Add lb etcd record

--- etcd-dns/main.tf
+++ etcd-dns/main.tf
@@ -1,5 +1,5 @@
 data "template_file" "custom_ipxe" {
-  count = "1"
+  count = "3"
   template = "${ file( "${ path.module }/custom_ipxe.yml" )}"
   vars {
     image = "${ var.image }${ count.index + 1}"
@@ -13,7 +13,7 @@ provider "packet" {
 
 
 resource "packet_device" "infra" {
-  count            = "1"
+  count            = "3"
   hostname         = "master-node${ count.index + 1 }"
   plan             = "baremetal_0"
   facility         = "ewr1"
@@ -26,7 +26,7 @@ resource "packet_device" "infra" {
 
 
 resource "null_resource" "etcd" {
-  count = "1"
+  count = "3"
 
   provisioner "local-exec" {
     when = "create"
@@ -40,6 +40,10 @@ resource "null_resource" "etcd" {
 
     curl -XPUT http://"${ var.etcd_server}"/v2/keys/skydns/io/goppa-internal/_tcp/_etcd-client/"${ element(packet_device.infra.*.hostname, count.index) }" \
     -d value='{"host":"${ element(packet_device.infra.*.hostname, count.index) }.goppa-internal.io","port":2379,"priority":0,"weight":0}'
+
+    curl -XPUT http://"${ var.etcd_server }"/v2/keys/skydns/io/goppa-internal/etcd/"${ element(packet_device.infra.*.hostname, count.index) }" \
+    -d value='{"host":"${ element(packet_device.infra.*.access_public_ipv4, count.index) }"}'
+
 EOF
   }
 
