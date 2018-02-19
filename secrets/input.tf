2571b6552e7184b7e116c1efb3c66f6ec8dab03b
denverwilliams <denver@debian.nz>
2018-02-19 14:21:10 +0000

    Update crts to support new cidr range

--- secrets/input.tf
+++ secrets/input.tf
@@ -17,7 +17,7 @@ variable "tls_client_cert_dns_names" { default = "kubernetes,kubernetes.default,
 variable "tls_apiserver_cert_subject_common_name" { default = "kubernetes-master" }
 variable "tls_apiserver_cert_validity_period_hours" { default = 1000 }
 variable "tls_apiserver_cert_early_renewal_hours" { default = 100 }
-variable "tls_apiserver_cert_ip_addresses" { default = "127.0.0.1,100.64.0.1,100.64.0.10" }
+variable "tls_apiserver_cert_ip_addresses" { default = "127.0.0.1,10.11.12.1,10.11.12.254" }
 variable "tls_apiserver_cert_dns_names" { default = "kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster.local,*.goppa-internal.io" }
 
 
